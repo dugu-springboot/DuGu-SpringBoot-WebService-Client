1d8c44404fcb0c07c11603bc2a9f9d2cd841bd14
simonxue <xueyao.me@gmail.com>
2021-09-13 22:42:48 +0800

    init

---
+++ src/main/java/me/xueyao/ws/ObjectFactory.java
@@ -0,0 +1,71 @@
+
+package me.xueyao.ws;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the me.xueyao.ws package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _EmrService_QNAME = new QName("http://service.webservice.example.com", "emrService");
+    private final static QName _EmrServiceResponse_QNAME = new QName("http://service.webservice.example.com", "emrServiceResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: me.xueyao.ws
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link EmrService }
+     * 
+     */
+    public EmrService createEmrService() {
+        return new EmrService();
+    }
+
+    /**
+     * Create an instance of {@link EmrServiceResponse }
+     * 
+     */
+    public EmrServiceResponse createEmrServiceResponse() {
+        return new EmrServiceResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link EmrService }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.webservice.example.com", name = "emrService")
+    public JAXBElement<EmrService> createEmrService(EmrService value) {
+        return new JAXBElement<EmrService>(_EmrService_QNAME, EmrService.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link EmrServiceResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.webservice.example.com", name = "emrServiceResponse")
+    public JAXBElement<EmrServiceResponse> createEmrServiceResponse(EmrServiceResponse value) {
+        return new JAXBElement<EmrServiceResponse>(_EmrServiceResponse_QNAME, EmrServiceResponse.class, null, value);
+    }
+
+}
---
+++ src/main/java/me/xueyao/ws/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.webservice.example.com")
+package me.xueyao.ws;
